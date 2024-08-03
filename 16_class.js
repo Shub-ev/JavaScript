a4eea81d429ad40afae89b00c4ecbf4e3e091305
shubhamg <shubhamg200204@gmail.com>
2024-08-03 17:04:17 +0530

    a

--- 16_class.js
+++ 16_class.js
@@ -1,12 +1,21 @@
 // classes works as the mold to the object
 // object are the instances of the class
 
+
+// ******   01   ******* classes
 class Person {
-    // name                 // no need to specify the memeber properties we can initialize or set member properties form
+    // name                 // no need to specify the memeber properties we can initialize or set member properties from
     // constructor
     constructor(name){  // constructor method
         this.name = name;  // setting member property 
     }
+
+    // The constructor is defined using the constructor keyword. Just like a constructor outside a class definition, it will:
+
+    // 01 . create a new object
+    // 02 . bind this to the new object, so you can refer to this in your constructor code
+    // 03 . run the code in the constructor
+    // 04 . return the new object.
 
     greet(){   // method/member function
         console.log(`Hello I am ${this.name}`);  
@@ -14,4 +23,50 @@ class Person {
 }
 
 let john = new Person("John Doe"); // creating instance of person class using constructor 
-john.greet();
+john.greet(); // calling a greet method of john object
+
+
+// ******   02   ******* inheritance
+// this class inherits all the properties and methods of parent class Person
+class Teacher extends Person{
+    constructor(name, sub){
+        super(name);
+        this.subject = sub;
+    }
+
+    greet() {  // this greets overrides base method greet and this method will be reffered for Teacher object
+        console.log(`Hello I am ${this.name} and I am a teacher of ${this.subject}`);
+    }
+}
+
+let mathTeacher = new Teacher("John", "Maths");
+mathTeacher.greet();
+
+
+
+// ******   03   ******* Encapsulation
+class Student extends Person{
+    // now here is the field we want to make private and dont allow to access directly from outside but 
+    // allow within the object/instance of class this is done by making that property private this is done by # operator
+
+    #religion;  // ** private data property **
+    
+    constructor(name, religion){
+        super(name);
+        this.#religion = religion;
+    }
+
+    #greet(str){   // this is a private method hence cannot be accessed from the outside
+        console.log(str);  
+    }
+
+    greetStudent(){
+        this.#greet(`Hello I am ${this.name} and I am a Student ${this.#religion}`)
+    }
+}
+
+let student1 = new Student("Hari", "Atheis");
+// student1.greet() if you do this it will print "Hello I am Hari" as greet is not accessible for student object ans it will access the parent's method 
+student1.greetStudent();
+console.log(student1.religion); // this will not work bcoz religion is private property and canot be accessed outside
+// console.log(student1.#religion);  accessing like this will give ("Private field '#religion' must be declared in an enclosing class") this error
---
+++ 18_asynchronous.js
@@ -0,0 +1,92 @@
+// JS executes on single thread by default
+
+// synchronous program is one executing one after another 
+// execution of next depends on the results of previous, or not
+// but this approach could freeze the functionality in case of heavy computing 
+
+// to avoid this we use asynchronous executing in which processes are done on the another thread
+// create and call such functions which will not block execution flow of main thread and will execute on another thread
+// and return the results of the execution paralally
+
+
+// **** there are several methods to achieve asynchronous execution ****
+
+
+// ***** 01 ***** call back functions 
+
+// in this method we use 
+// 1. call back 
+// 2. promises
+// which returns a value once called and before the execution completion so other tasks will execute real fine
+
+// Before the widespread adoption of the modern fetch API and async/await syntax in JavaScript, 
+// the XMLHttpRequest (XHR) object was the primary means of making asynchronous HTTP requests.
+
+// An event handler is a particular type of callback. A callback is just a function that's passed into another function, 
+// with the expectation that the callback will be called at the appropriate time.
+// call back inside a call back makes pyramid and gets complicated hence it is also called as 
+
+
+// 1. pyramid of Doom           or             // 2 . call back hell
+// i have not implemented asynchronous using calls backs
+
+
+
+
+// ***** 02 ***** Promises
+
+// promises are the foundation of asynchronous programming in moder javascript.
+// promise is the object returned by an async. function which represents the current state of the function (ex . pending etc.)
+// With a promise-based API, the asynchronous function starts the operation and returns a Promise object. 
+// You can then attach handlers to this promise object, and these handlers will be executed when the operation has succeeded or failed.
+
+const fetchPromise = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
+console.log(fetchPromise);
+
+fetchPromise.then((response) => {   // passing a handler function into the Promise's then() method.
+  console.log(`Received response: ${response.status}`);
+});
+
+console.log("Started request…");
+
+// Chaining promises => when you deal with another promise when you are currenly in a promise then it is chaining promises
+
+const fetchPromise2 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+)
+fetchPromise2.then(response => {
+    const jsonPromise = response.json();
+    jsonPromise.then(response => { // this is chaining promises
+        console.log(response[0]);
+    })
+})
+// above code can also be written as
+
+const fetchPromise3 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+)
+.then(response => {
+    if(!response.ok){
+        throw new Error("Error");
+    }
+
+    return response.json();
+})
+.then(response => {
+    console.log(response[0]);
+})
+
+// To support error handling, Promise objects provide a catch() method.
+// the handler passed to then() is called when the asynchronous operation succeeds, 
+// the handler passed to catch() is called when the asynchronous operation fails.
+
+// ** Promise Terminology **
+// state of a primise
+// A. Pending => the promise has been created, and the asynchronous function it's associated with has not succeeded or failed yet.
+// B. fulfilled => the asynchronous function has succeeded. When a promise is fulfilled, its then() handler is called.
+// C. rejected => the asynchronous function has failed. When a promise is rejected, its catch() handler is called.
+
+
+// ***** 02 ***** async and await
