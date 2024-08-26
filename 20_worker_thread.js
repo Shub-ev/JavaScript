0b2f1360b7039fded72741e3570eb893cdc448ea
shubhamg <shubhamg200204@gmail.com>
2024-08-26 11:54:29 +0530

    a

---
+++ 20_worker_thread.js
@@ -0,0 +1,6 @@
+// Workers give you the ability to run some tasks in a different thread
+// There are three different sorts of workers:
+// 1. dedicated workers
+// 2. shared workers
+// 3. service workers
+
