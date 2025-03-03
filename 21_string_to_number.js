a675492dbdf7d457a696ec47c42f1f8d1314c3d1
Shub-ev <shubhamg200204@gmail.com>
2025-03-03 19:51:28 +0530

    a

---
+++ 21_string_to_number.js
@@ -0,0 +1,27 @@
+let string = "1234";
+
+// 1. parseInt
+// syntax => parseInt(string, radix)
+
+console.log(parseInt(string));
+console.log(typeof parseInt(string));
+// but this dosent work with decimal string numbers
+string = "1234.1234adsfasdf"
+console.log(parseInt(string));  // output => 1234
+console.log(typeof parseInt(string));
+
+
+// 2. parseFloat
+// syntax => parseFloat(string)
+
+console.log(parseFloat(string));
+console.log(typeof parseFloat(string));
+string = 1234.0;
+console.log(parseFloat(string));
+console.log(typeof parseFloat(string));
+
+string = "1111" // binary string
+console.log(parseInt(string, 2));
+
+// 3. add + prior to string number
+console.log(typeof (+"1234"));
